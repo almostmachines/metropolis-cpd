e2edb4ee33a208b1e62d2ffb4b7bf00698cecadc
John King <jking@almostmachines.dev>
2026-02-13 18:09:12 +0800

    Revert "Create library"
    This reverts commit 7fac6a4056e9a1c0b3e16babf1041fc051bd600e.

--- src/lib.ts
+++
@@ -1,5 +0,0 @@
-import './lib.css';
-
-export { default as MetropolisCPD } from './App';
-export { DEFAULT_CONFIG } from './types';
-export type { AlgorithmConfig, Params, DataPoint, PriorMuMeans, PriorMuStds } from './types';
